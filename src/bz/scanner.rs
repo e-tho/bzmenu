ab772b614ce0271732b5061aa0eeaf4a60735cf7
e-tho <etho.ledroy@pm.me>
2025-03-31 11:17:14 +0200

    Add device discovery stop on scan in progress notification default action

--- src/bz/scanner.rs
+++ src/bz/scanner.rs
@@ -3,12 +3,14 @@ use bluer::Adapter;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use tokio::sync::mpsc::UnboundedSender;
-use tokio::time::Duration;
+use tokio::{spawn, sync::Mutex, task::JoinHandle, time::Duration};
 
+#[derive(Clone)]
 pub struct Scanner {
     adapter: Arc<Adapter>,
     is_scanning: Arc<AtomicBool>,
     log_sender: UnboundedSender<String>,
+    scan_task: Arc<Mutex<Option<JoinHandle<()>>>>,
 }
 
 impl Scanner {
@@ -21,6 +23,7 @@ impl Scanner {
             adapter,
             is_scanning,
             log_sender,
+            scan_task: Arc::new(Mutex::new(None)),
         }
     }
 
@@ -47,7 +50,7 @@ impl Scanner {
         let is_scanning = self.is_scanning.clone();
         let log_sender = self.log_sender.clone();
 
-        tokio::spawn(async move {
+        let task = spawn(async move {
             tokio::time::sleep(Duration::from_secs(timeout_sec)).await;
 
             is_scanning.store(false, Ordering::Relaxed);
@@ -58,6 +61,34 @@ impl Scanner {
 
             drop(discovery_stream);
         });
+
+        let mut scan_task_guard = self.scan_task.lock().await;
+        *scan_task_guard = Some(task);
+
+        Ok(())
+    }
+
+    pub async fn stop_discovery(&self) -> Result<()> {
+        if !self.is_scanning.load(Ordering::Relaxed) {
+            try_send_log!(
+                self.log_sender,
+                "No Bluetooth discovery in progress to stop".to_string()
+            );
+            return Ok(());
+        }
+
+        try_send_log!(
+            self.log_sender,
+            "Stopping Bluetooth discovery...".to_string()
+        );
+
+        self.is_scanning.store(false, Ordering::Relaxed);
+
+        let mut scan_task_guard = self.scan_task.lock().await;
+        if let Some(task) = scan_task_guard.take() {
+            task.abort();
+            try_send_log!(self.log_sender, "Discovery task aborted".to_string());
+        }
 
         Ok(())
     }
