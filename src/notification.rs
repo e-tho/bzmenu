85d5d98fe0cf71fa695f1ff83c95d87a85c9254f
e-tho <128100160+e-tho@users.noreply.github.com>
2025-04-09 18:33:52 +0200

    Refactor progress notification method for generic and customizable use

--- src/notification.rs
+++ src/notification.rs
@@ -130,20 +130,33 @@ impl NotificationManager {
     // "Allow an on_close handler without consuming the NotificationHandle"
     // This would simplify our implementation by avoiding the need for a separate thread
     // and allowing us to update the notification directly after setting up the on_close handler.
-    pub fn send_scan_progress_notification(
+    pub fn send_progress_notification(
         &self,
-        scan_duration_sec: u64,
+        duration_sec: u64,
         on_cancel: impl FnOnce() + Send + 'static,
+        progress_summary: Option<String>,
+        progress_body: Option<String>,
+        progress_icon: Option<&str>,
+        completion_summary: Option<String>,
+        completion_body: Option<String>,
+        completion_icon: Option<&str>,
     ) -> Result<u32> {
-        let icon_name = self.icons.get_xdg_icon("bluetooth");
+        let progress_icon = self
+            .icons
+            .get_xdg_icon(progress_icon.unwrap_or("bluetooth"));
+        let progress_summary = progress_summary.unwrap_or_else(|| String::from("BlueZ Menu"));
+        let progress_body = progress_body.unwrap_or_else(|| String::from(""));
 
-        let summary = t!("BlueZ Menu");
-        let body = t!("notifications.bt.scan_in_progress");
+        let completion_icon = self
+            .icons
+            .get_xdg_icon(completion_icon.unwrap_or("bluetooth"));
+        let completion_summary = completion_summary.unwrap_or_else(|| String::from("BlueZ Menu"));
+        let completion_body = completion_body.unwrap_or_else(|| String::from(""));
 
         let notification_handle = Notification::new()
-            .summary(&summary)
-            .body(&body)
-            .icon(&icon_name)
+            .summary(&progress_summary)
+            .body(&progress_body)
+            .icon(&progress_icon)
             .timeout(Timeout::Never)
             .hint(Hint::Transient(true))
             .hint(Hint::Category("progress".to_string()))
@@ -153,16 +166,20 @@ impl NotificationManager {
         let id = notification_handle.id();
 
         spawn({
-            let summary = summary.clone();
-            let body = body.clone();
-            let icon_name = icon_name.clone();
+            let progress_summary = progress_summary.clone();
+            let progress_body = progress_body.clone();
+            let progress_icon = progress_icon.clone();
+
+            let completion_icon = completion_icon.clone();
+            let completion_summary = completion_summary.clone();
+            let completion_body = completion_body.clone();
 
             let on_cancel_wrapped = Arc::new(Mutex::new(Some(Box::new(on_cancel))));
 
             move || {
                 let start_time = std::time::Instant::now();
                 let update_interval = std::time::Duration::from_millis(500);
-                let total_duration = std::time::Duration::from_secs(scan_duration_sec);
+                let total_duration = std::time::Duration::from_secs(duration_sec);
 
                 let cancelled = Arc::new(AtomicBool::new(false));
                 let cancelled_for_loop = cancelled.clone();
@@ -194,9 +211,9 @@ impl NotificationManager {
 
                     let update_result = Notification::new()
                         .id(id)
-                        .summary(&summary)
-                        .body(&body)
-                        .icon(&icon_name)
+                        .summary(&progress_summary)
+                        .body(&progress_body)
+                        .icon(&progress_icon)
                         .timeout(Timeout::Never)
                         .hint(Hint::Transient(true))
                         .hint(Hint::Category("progress".to_string()))
@@ -218,9 +235,9 @@ impl NotificationManager {
                 if start_time.elapsed() >= total_duration {
                     let _ = Notification::new()
                         .id(id)
-                        .summary(&summary)
-                        .body(&t!("notifications.bt.scan_completed"))
-                        .icon(&icon_name)
+                        .summary(&completion_summary)
+                        .body(&completion_body)
+                        .icon(&completion_icon)
                         .timeout(Timeout::Milliseconds(2000))
                         .show();
                 }
